ca03ee4389cef99b24082c95e359d7573021fa00
Subhra Das <subhra.das@synapse.com>
2020-08-18 02:39:16 +0530

    resolved code smell :Add a private constructor to hide the implicit public one

--- src/main/java/com/ekocaman/config/Constants.java
+++ src/main/java/com/ekocaman/config/Constants.java
@@ -3,7 +3,7 @@ package com.ekocaman.config;
 public class Constants {
     public static final int ERROR_CODE_ILLEGAL_PARAMETER = 1;
     public static final int ERROR_CODE_OVERFLOW = 2;
- //THis is a private constructor   
+ 
     private Constants(){
     	
     }
