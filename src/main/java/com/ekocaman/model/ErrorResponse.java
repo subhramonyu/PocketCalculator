5ea1fbea8bba771d36db20ee1928f75ee064c406
Subhra Das <subhra.das@synapse.com>
2020-08-18 01:20:04 +0530

    init Project

---
+++ src/main/java/com/ekocaman/model/ErrorResponse.java
@@ -0,0 +1,28 @@
+package com.ekocaman.model;
+
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlRootElement
+public class ErrorResponse {
+    @XmlElement(name = "error_code")
+    private int errorCode;
+    private String message;
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
