c470adaea75e161e4fa066bea63210027867c168
Subhra Das <subhra.das@synapse.com>
2020-08-18 09:13:29 +0530

    test

--- src/main/java/com/ekocaman/config/Constants.java
+++ src/main/java/com/ekocaman/config/Constants.java
@@ -3,7 +3,7 @@ package com.ekocaman.config;
 public class Constants {
     public static final int ERROR_CODE_ILLEGAL_PARAMETER = 1;
     public static final int ERROR_CODE_OVERFLOW = 2;
- 
+    //Private Constructor for restiction
     private Constants(){
     	
     }
